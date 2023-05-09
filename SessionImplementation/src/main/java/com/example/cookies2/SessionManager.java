be08b9b31a7ac085c259aeb5acedf533df8f6aaf
matteobregola <matteobregola@gmail.com>
2023-05-09 12:00:06 +0200

    frontend 2.0, backend 1.5

--- SessionImplementation/src/main/java/com/example/cookies2/SessionManager.java
+++ SessionImplementation/src/main/java/com/example/cookies2/SessionManager.java
@@ -1,11 +1,12 @@
 package com.example.cookies2;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 @WebServlet(name = "SessionManager", value = "/SessionManager")
 public class SessionManager extends HttpServlet {
@@ -22,27 +23,23 @@ public class SessionManager extends HttpServlet {
         -Tecnici (JSESSION ID salvato automaticamente dal browser, aggiungo alla session il parametro tecnici)
         -Tutti (JSESSION ID salvato automaticamente dal browser)
          */
-        String scelta= (Arrays.toString(request.getParameterValues("opzione")));
+        String scelta = request.getParameter("opzione");
+        System.out.println(scelta);
 
         HttpSession session = request.getSession();
         request.removeAttribute("formNeeded");
-        request.setAttribute("updated",true);
+        request.setAttribute("updated", true);
         String origin = request.getHeader("referer");
-        String encodedURL=origin;
+        String encodedURL = origin;
 
-        if(scelta.equals("Disattivati")){
-            encodedURL=response.encodeURL(origin);
+        if (scelta.equals("Disattivati")) {
+            encodedURL = response.encodeURL(origin);
             /* in realtà questo funziona correttamente se i cookies sono disabilitati, se sono
             abilitati funziona come gli altri, si può togliere che tanto non era un requisito
-
              */
+        } else if (scelta.equals("Tecnici")) {
+            session.setAttribute("Tecnici", "true");
         }
-        else{
-            if (scelta.equals("Tecnici")){
-                session.setAttribute("Tecnici", "true");
-            }
-        }
-        //System.out.println(encodedURL);
         response.sendRedirect(encodedURL);
     }
 }
