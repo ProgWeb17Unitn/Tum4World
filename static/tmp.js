369894db17899e890d7083f60a6e222a86620d71
Riccardo Libanora <riccardo.libanora@gmail.com>
2023-05-10 17:46:04 +0200

    removed old style setting, added header features, cleaned up some code

--- static/tmp.js
+++ static/tmp.js
@@ -1,67 +1,145 @@
-var N = 0;
-
+activePalette = 0;
 palette = {};
-palette[0] = {
-    'primary_l': '#7dbf63', 'primary_d': '#97cc82',
-    'secondary_l': '#9ce0e4', 'secondary_d': '#83d8dd',
-    'tertiary': 'white'
-};
-palette[1] = {
-    'primary_l': '#7CB518', 'primary_d': '#5C8001',
-    'secondary_l': '#F3DE2C', 'secondary_d': '#FBB02D',
-    'tertiary': 'white'
-};
-palette[2] = {
-    'primary_l': '#38A3A5', 'primary_d': '#22577A',
-    'secondary_l': '#80ED99', 'secondary_d': '#57CC99',
-    'tertiary': '#C7F9CC'
-};
-palette[3] = {
-    'primary_l': '#F49E4C', 'primary_d': '#AB3428',
-    'secondary_l': '#3B8EA5', 'secondary_d': '#2D728F',
-    'tertiary': '#F5EE9E'
-};
-palette[4] = {
-    'primary_l': '#F4D35E', 'primary_d': '#0D3B66',
-    'secondary_l': '#F4D35E', 'secondary_d': '#EE964B',
-    'tertiary': '#FAF0CA'
-};
+page = {};
+menu = {};
 
+page.load = function () {
+    menu.status = 'closed';
+    page.status = 'landscape';
 
-function switch_style(n) {
-    N = n;
-    const style = document.getElementsByTagName('style')[0];
+    if (window.innerWidth > 960)
+        page.toggle('landscape');
+    else
+        page.toggle('portrait');
+
+    palette[1] = {
+        'primary_l': '#7CB518', 'primary_d': '#5C8001',
+        'secondary_l': '#F3DE2C', 'secondary_d': '#FBB02D',
+        'tertiary': 'white'
+    };
+    palette[2] = {
+        'primary_l': '#38A3A5', 'primary_d': '#22577A',
+        'secondary_l': '#80ED99', 'secondary_d': '#57CC99',
+        'tertiary': '#C7F9CC'
+    };
+    palette[3] = {
+        'secondary_l': '#F49E4C', 'secondary_d': '#AB3428',
+        'primary_l': '#3B8EA5', 'primary_d': '#2D728F',
+        'tertiary': '#F5EE9E'
+    };
+    palette[4] = {
+        'primary_l': '#F4D35E', 'primary_d': '#0D3B66',
+        'secondary_l': '#F4D35E', 'secondary_d': '#EE964B',
+        'tertiary': '#FAF0CA'
+    };
+
+    document.getElementById('hamburger').addEventListener('click', function () {
+        if (menu.status === 'closed')
+            menu.open();
+        else
+            menu.close();
+    });
+
+    switchStyle(1);
+}
+function switchStyle(n) {
+    window.scrollTo(0, 0);
+    activePalette = n;
     const icon = document.getElementsByTagName('link')[0];
     const logo = document.getElementById('logo');
     const background = document.getElementsByTagName('body')[0];
-    const texts = document.getElementsByClassName('colored_text');
-
+    const download = document.getElementById('download');
+    const links = document.getElementsByTagName('a');
+    const texts = document.getElementsByClassName('coloredText');
 
     icon.setAttribute('href', 'assets/option' + n + '/logo.svg');
     logo.setAttribute('src', 'assets/option' + n + '/logo.svg');
-    background.setAttribute('style', 'background-image: url(assets/option'+n+'/background.jpg);');
+    background.style.backgroundImage = 'url(assets/option' + n + '/background.jpg)';
 
+    document.getElementsByTagName('footer')[0].style.backgroundColor = palette[n].primary_d;
+    download.style.backgroundColor = palette[n].secondary_d;
 
-    document.getElementsByTagName('footer')[0].setAttribute('style', 'background-color:'+palette[N].primary_d+';');
-    document.getElementsByTagName('button')[0].setAttribute('style', 'background-color:'+palette[N].secondary_d+';');
+    download.addEventListener('mouseover', function () {
+        download.style.backgroundColor = palette[n].secondary_l;
+    });
 
-    style.innerText = 'main button:hover{background-color:' + palette[N].primary_l + ';}\n'
-        + 'nav a:hover {color:'+palette[n].secondary_l+'}\n'
-        + '.colored_text {color:'+palette[n].tertiary+';}';
+    download.addEventListener('mouseout', function () {
+        download.style.backgroundColor = palette[n].secondary_d;
+    });
+
+    for (let i of texts)
+        i.style.color = palette[n].tertiary;
+
+    for (let i of links) {
+        i.addEventListener('mouseover', function () {
+            i.style.color = palette[n].secondary_l;
+        });
+
+        i.addEventListener('mouseout', function () {
+            i.style.color = palette[n].tertiary;
+        });
+    }
 }
 
 window.onscroll = function () {
-    let header = document.getElementsByTagName('header')[0];
+    const header = document.getElementsByTagName('header')[0];
+    if (menu.status === 'open')
+        return;
 
     let ref = document.getElementById('index').offsetTop - header.clientHeight;
-    //calcolo come punto di riferimento il punto in cui termina l'header
-
     let pos = window.scrollY;
-    //quanto ho scrollato la pagina per ora
 
-    if (ref > pos)
-        header.setAttribute('style', 'background-color:transparent;');
-    else
-        //ho scrollato fino a che il testo arriva all'header, quindi devo renderlo opaco
-        header.setAttribute('style', 'background-color:' + palette[N].primary_d + ';');
+    if (ref > pos) {
+        header.style.backgroundColor = 'transparent';
+        header.style.height = '6vh';
+    } else {
+        header.style.backgroundColor = palette[activePalette].primary_d;
+        header.style.height = '4vh';
+    }
 };
+
+window.onresize = function () {
+        menu.close();
+    if (window.innerWidth > 960)
+        page.toggle('landscape');
+    else
+        page.toggle('portrait');
+}
+
+menu.open = function () {
+    const nav = document.getElementsByTagName('nav')[0];
+    const header = document.getElementsByTagName('header')[0];
+    header.style.height = 'min-content';
+    header.style.backgroundColor = palette[activePalette].primary_d;
+    header.style.flexDirection = 'column';
+    nav.style.display = 'block';
+    nav.style.alignSelf = 'flex-start';
+    nav.style.fontSize = '100%';
+    menu.status = 'open';
+}
+
+menu.close = function () {
+    const nav = document.getElementsByTagName('nav')[0];
+    const header = document.getElementsByTagName('header')[0];
+    header.style.height = '6vh';
+    header.style.flexDirection = 'row';
+    nav.style.display = 'none';
+    nav.style.alignSelf = 'center';
+    nav.style.fontSize = '200%';
+    window.scrollTo(window.scrollX, window.scrollY - 1);
+    menu.status = 'closed';
+}
+
+page.toggle = function (mode) {
+    const nav = document.getElementsByTagName('nav')[0];
+    const hamburger = document.getElementById('hamburger');
+
+    if (mode === 'landscape') {
+        hamburger.style.display = 'none';
+        nav.style.display = 'block';
+    } else {
+        hamburger.style.display = 'block';
+        nav.style.display = 'none';
+    }
+    self.status = mode;
+}
