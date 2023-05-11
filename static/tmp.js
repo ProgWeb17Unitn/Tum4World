00687121627336c78a626c470dc4cc366b41a492
Riccardo Libanora <riccardo.libanora@gmail.com>
2023-05-11 11:11:20 +0200

    added quotes section, removed temporary styles and set up definitive theme switcher

--- static/tmp.js
+++ static/tmp.js
@@ -1,36 +1,33 @@
 activePalette = 0;
+activePath = '';
 palette = {};
 page = {};
 menu = {};
+quotes = {};
 
 page.load = function () {
     menu.status = 'closed';
     page.status = 'landscape';
 
-    if (window.innerWidth > 960)
-        page.toggle('landscape');
+    if (window.innerWidth > 1000)
+        page.toggleView('landscape');
     else
-        page.toggle('portrait');
+        page.toggleView('portrait');
 
+    palette[0] = {
+        'primary_l': '#', 'primary_d': '#',
+        'secondary_l': '#', 'secondary_d': '#',
+        'tertiary': '#'
+    };
     palette[1] = {
-        'primary_l': '#7CB518', 'primary_d': '#5C8001',
-        'secondary_l': '#F3DE2C', 'secondary_d': '#FBB02D',
-        'tertiary': 'white'
-    };
-    palette[2] = {
-        'primary_l': '#38A3A5', 'primary_d': '#22577A',
-        'secondary_l': '#80ED99', 'secondary_d': '#57CC99',
-        'tertiary': '#C7F9CC'
-    };
-    palette[3] = {
         'secondary_l': '#F49E4C', 'secondary_d': '#AB3428',
         'primary_l': '#3B8EA5', 'primary_d': '#2D728F',
         'tertiary': '#F5EE9E'
     };
-    palette[4] = {
-        'primary_l': '#F4D35E', 'primary_d': '#0D3B66',
-        'secondary_l': '#F4D35E', 'secondary_d': '#EE964B',
-        'tertiary': '#FAF0CA'
+    palette[2] = {
+        'primary_l': '#', 'primary_d': '#',
+        'secondary_l': '#', 'secondary_d': '#',
+        'tertiary': '#'
     };
 
     document.getElementById('hamburger').addEventListener('click', function () {
@@ -40,43 +37,56 @@ page.load = function () {
             menu.close();
     });
 
-    switchStyle(1);
+    page.switchStyle(1);
 }
-function switchStyle(n) {
+page.switchStyle = function (n) {
     window.scrollTo(0, 0);
+
     activePalette = n;
+    if (n === 0)
+        activePath = 'admin';
+    else if (n === 1)
+        activePath = 'dark';
+    else
+        activePath = 'light';
+
     const icon = document.getElementsByTagName('link')[0];
     const logo = document.getElementById('logo');
+    const helper = document.getElementById('helper');
     const background = document.getElementsByTagName('body')[0];
     const download = document.getElementById('download');
     const links = document.getElementsByTagName('a');
     const texts = document.getElementsByClassName('coloredText');
+    const quote = document.getElementById('quotes').getElementsByTagName('p')[0];
 
-    icon.setAttribute('href', 'assets/option' + n + '/logo.svg');
-    logo.setAttribute('src', 'assets/option' + n + '/logo.svg');
-    background.style.backgroundImage = 'url(assets/option' + n + '/background.jpg)';
+    icon.setAttribute('href', 'assets/' + activePath + '/logo.svg');
+    logo.setAttribute('src', 'assets/' + activePath + '/logo.svg');
+    helper.setAttribute('src', 'assets/' + activePath + '/logo.svg');
+    background.style.backgroundImage = 'url(assets/' + activePath + '/background.jpg)';
 
-    document.getElementsByTagName('footer')[0].style.backgroundColor = palette[n].primary_d;
-    download.style.backgroundColor = palette[n].secondary_d;
+    quote.style.backgroundColor = palette[activePalette].tertiary;
+    quote.style.color = palette[activePalette].secondary_l;
+    document.getElementsByTagName('footer')[0].style.backgroundColor = palette[activePalette].primary_d;
+    download.style.backgroundColor = palette[activePalette].secondary_d;
 
     download.addEventListener('mouseover', function () {
-        download.style.backgroundColor = palette[n].secondary_l;
+        download.style.backgroundColor = palette[activePalette].secondary_l;
     });
 
     download.addEventListener('mouseout', function () {
-        download.style.backgroundColor = palette[n].secondary_d;
+        download.style.backgroundColor = palette[activePalette].secondary_d;
     });
 
     for (let i of texts)
-        i.style.color = palette[n].tertiary;
+        i.style.color = palette[activePalette].tertiary;
 
     for (let i of links) {
         i.addEventListener('mouseover', function () {
-            i.style.color = palette[n].secondary_l;
+            i.style.color = palette[activePalette].secondary_l;
         });
 
         i.addEventListener('mouseout', function () {
-            i.style.color = palette[n].tertiary;
+            i.style.color = palette[activePalette].tertiary;
         });
     }
 }
@@ -100,15 +110,16 @@ window.onscroll = function () {
 
 window.onresize = function () {
         menu.close();
-    if (window.innerWidth > 960)
-        page.toggle('landscape');
+    if (window.innerWidth > 1000)
+        page.toggleView('landscape');
     else
-        page.toggle('portrait');
+        page.toggleView('portrait');
 }
 
 menu.open = function () {
     const nav = document.getElementsByTagName('nav')[0];
     const header = document.getElementsByTagName('header')[0];
+    const title = document.getElementsByTagName('h1')[0];
     header.style.height = 'min-content';
     header.style.backgroundColor = palette[activePalette].primary_d;
     header.style.flexDirection = 'column';
@@ -121,6 +132,7 @@ menu.open = function () {
 menu.close = function () {
     const nav = document.getElementsByTagName('nav')[0];
     const header = document.getElementsByTagName('header')[0];
+    const title = document.getElementsByTagName('h1')[0];
     header.style.height = '6vh';
     header.style.flexDirection = 'row';
     nav.style.display = 'none';
@@ -130,7 +142,7 @@ menu.close = function () {
     menu.status = 'closed';
 }
 
-page.toggle = function (mode) {
+page.toggleView = function (mode) {
     const nav = document.getElementsByTagName('nav')[0];
     const hamburger = document.getElementById('hamburger');
 
@@ -142,4 +154,31 @@ page.toggle = function (mode) {
         nav.style.display = 'none';
     }
     self.status = mode;
+}
+
+quotes.init = function (){
+    quotes[0] = 'questa frase è una frase';
+    quotes[1] = 'ho davvero poca fantasia';
+    quotes[2] = 'qualcuno potrebbe disegnarmi degli occhi?';
+    quotes[3] = 'non fa ridere';
+    quotes[4] = 'Crispo è il mio migliore amico';
+    setInterval(quotes.generate, 20000);
+}
+
+quotes.generate = function (){
+    let i = Math.floor(Math.random()*5);
+    const box = document.getElementById('quotes').getElementsByTagName('p')[0];
+
+    box.innerText = quotes[i];
+    box.style.marginBottom = '2vh';
+    setTimeout(function (){
+        box.style.marginBottom = '1vh';
+    }, 1000);
+}
+
+page.toggleStyle = function () {
+    if (activePalette === 1)
+        page.switchStyle(2);
+    else if (activePalette === 2)
+        page.switchStyle(1);
 }
