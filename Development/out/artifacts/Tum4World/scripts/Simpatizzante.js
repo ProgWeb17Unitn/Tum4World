ba2b7eeec36a9fc9c0fa9d54917bb708bc979208
matteobregola <matteobregola@gmail.com>
2023-06-29 08:51:41 +0200

    fixed out

--- Development/out/artifacts/Tum4World/scripts/Simpatizzante.js
+++
@@ -1,9 +0,0 @@
-var value=0;
-function handleScroll() {
-    if(value===0){
-        theme.switch("Simpatizzante");
-        value++;
-    }
-    baseOnscroll();
-}
-window.addEventListener("scroll", handleScroll);
